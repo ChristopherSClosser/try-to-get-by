b2d84c0f345b80736f86ce584c065a6fb17cfb8f
Chris Closser <chrisclosser1@gmail.com>
2018-01-12 19:42:02 -0800

    testing and travis

--- intell/tests.py
+++ intell/tests.py
@@ -1,3 +1,24 @@
+"""Test intell."""
+
 from django.test import TestCase
 
-# Create your tests here.
+from getby.views import HomeView
+
+from . import views
+
+
+class ProfileTestCase(TestCase):
+    """."""
+
+    def setUp(self):
+        """."""
+
+    def test_home_view_has_title(self):
+        """."""
+        response = HomeView()
+        assert response.template_name == 'getby/homepage.html'
+
+    def test_intell_view_has_title(self):
+        """."""
+        response = views.index(self)
+        assert response.content == b"Hello, world. You're at the intell index."
