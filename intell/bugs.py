fe70708df7950c25a55a486cb17362254b82a04c
Chris Closser <chrisclosser1@gmail.com>
2018-01-15 13:54:49 -0800

    working on start and location finder

--- intell/bugs.py
+++ intell/bugs.py
@@ -27,12 +27,35 @@ class Bug(object):
     """Make a bug."""
 
     def __init__(self, id):
-        """."""
+        """Set defaults."""
         self.id = id
         self.position = None
         self.moving = False
-        self.direction = ''
+        self.directions = []
         self.space = None
+
+    def Move(self, mtx):
+        """
+        Determine how bug is to perform.
+
+        When called bug will be a part of the matrix passed in...
+        Bug needs to know where it can move ie. N, NE, E, SE, S, SW, W, NW
+        ...cannot move in negative index direction
+        [
+            [[], [], []],  >if bug1 is @ mtx[0][0] - directions = [E, SE, S]
+            [[], [], []],   if bug2 @ mtx[0][1] bug1 directions = [SE, S] ect..
+            [[], [], []],  >if bug1 is @ mtx[0][2] - directions = [W, SW, S]
+        ]                   if bug2 @ mtx[0][1] bug1 directions = [SW, S] ect..
+        """
+
+    def location(self, mtx):
+        """Location of bug in matrix."""
+        self.mtx = mtx
+        self.idx = []
+        for subarray in self.mtx:
+            if [self.id] in subarray:
+                self.idx.append((self.mtx.index(subarray), subarray.index([self.id])))
+        pass
 
 
 def start(bugs=2, size='small'):
@@ -40,7 +63,10 @@ def start(bugs=2, size='small'):
     grid = Matrix(size)
     for bug in range(bugs):
         rand_idx = random.randint(0, len(grid.mtx))
-        grid.mtx[rand_idx][rand_idx].append(Bug(bug))
+        new = Bug(bug)
+        grid.mtx[rand_idx][rand_idx].append(new.id)
+        new.location(grid.mtx)
+        import pdb; pdb.set_trace()
     return grid
 
 
