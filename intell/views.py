65cc41eb8871cffd6d3c9b3f7e0679ce340cf8da
Chris Closser <chrisclosser1@gmail.com>
2018-01-24 10:10:20 -0800

    messing with how matrix is displayed (it was wrong)

--- intell/views.py
+++ intell/views.py
@@ -7,5 +7,5 @@ from .bugs import start
 
 def index(request):
     """."""
-    mtx = start(bugs=9, size=10)
+    mtx = start(size=10)
     return render(request, 'getby/matrix.html', {'matrix': mtx.mtx})
