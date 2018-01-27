76a10dad90dce82a8ce48c4031bf3dd55bb16991
Chris Closser <chrisclosser1@gmail.com>
2018-01-27 12:34:07 -0800

    front end displaying the movements on click of my input button need to make it automatic... looks like ill have to use JQuery

--- intell/views.py
+++ intell/views.py
@@ -3,9 +3,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .bugs import start
+from django.views.generic import TemplateView
+
+MTX = start(size=10)
 
 
 def index(request):
     """."""
-    mtx = start(size=10)
-    return render(request, 'getby/matrix.html', {'matrix': mtx.mtx})
+    if request.method == 'GET':
+        MTX._bugs[0][1]._move_all_random()
+        return render(request, 'getby/matrix.html', {'matrix': MTX.mtx})
